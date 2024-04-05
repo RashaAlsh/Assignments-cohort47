670c3e7bba717fd6cd722c05c9e01e056fb6eeee
Rasha <157126609+RashaAlsh@users.noreply.github.com>
2024-04-05 11:26:21 +0200

    Update index.js

--- 3-UsingAPIs/Week2/assignment/ex1-programmerFun/index.js
+++ 3-UsingAPIs/Week2/assignment/ex1-programmerFun/index.js
@@ -1,68 +1,39 @@
 'use strict';
-/*------------------------------------------------------------------------------
-Full description at: https://github.com/HackYourFuture/Assignments/blob/main/3-UsingAPIs/Week2/README.md#exercise-1-programmer-fun
 
-1. Complete the function `requestData()` using `fetch()` to make a request to 
-   the url passed to it as an argument. The function should return a promise. 
-   Make sure that the promise is rejected in case of HTTP or network errors.
-2. Notice that the function `main()` calls `requestData()`, passing it the url 
-   `https://xkcd.now.sh/?comic=latest`. Try and run the code in the browser and 
-   open the browser's console to inspect the data returned from the request.
-3. Next, complete the function `renderImage()` to render an image as an `<img>` 
-   element appended to the document's body, using the data returned from the API.
-4. Complete the function `renderError()` to render any errors as an `<h1>` 
-   element appended to the document's body.
-5. Refactor the `main()` function to use `async/await`.
-6. Test error handling, for instance, by temporarily changing the `.sh` in the 
-   url with `.shx`. There is no server at the modified url, therefore this 
-   should result in a network (DNS) error.
-------------------------------------------------------------------------------*/
-async function requestData(url) {
-
-
-  try {
-      const response = await fetch(url);
-
-      if (!response.ok) {
-          throw new Error(`Network error: ${response.status} ${response.statusText}`);
-      }
-
-      const data = response.json();
-      return data;
-
-  } catch (err) {
-      console.log(`Error: ${err}`);
-      throw err;
-  }
+function requestData(url) {
+  return fetch(url)
+  .then((result)=>{
+    if(!result.ok){
+      throw new Error("something went wrong")
+    }
+    return result.json();
+  })
 }
 
-
 function renderImage(data) {
+  const myImg = document.createElement("img");
+  myImg.src = data.img;
+  myImg.alt = "img"
 
-  const img = document.createElement('img');
-  img.src = data.img;
-  document.body.appendChild(img);
-
-  console.log(data);
+  document.body.appendChild(myImg);
 }
 
 function renderError(error) {
-  const errh1 = document.createElement(`h1`);
-  errh1.textContent = error;    
-  document.body.appendChild(errh1);
+  const myHeader = document.createElement("h1");
+  const myHeaderText = document.createTextNode(`${error}`);
 
-  console.log(error);
+  myHeader.appendChild(myHeaderText);
+  document.body.appendChild(myHeader);
 }
 
+
 async function main() {
-  try {
-      const dataURL = await requestData('https://xkcd.now.sh/?comic=latest');
-
-      renderImage(dataURL);
-
-  } catch (err) {
-      renderError(err);
+  try{
+    const response = await requestData('https://xkcd.now.sh/?comic=latest');
+    renderImage(response);
+  }catch(error){
+    renderError(error);
   }
 }
 
-window.addEventListener('load', main)
+window.addEventListener('load', main);
